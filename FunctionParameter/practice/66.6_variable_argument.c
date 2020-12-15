804d9a45a155146b0378926194592138f940fc5e
eezn <pplaneoo@gmail.com>
2020-12-15 15:07:17 +0900

    parameter2

---
+++ FunctionParameter/practice/66.6_variable_argument.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum(int args, ...)
+{
+    va_list ap;
+    int i = 0;
+    int result = 0;
+
+    va_start(ap, args);
+    while (i < args)
+    {
+        result += va_arg(ap, int);
+        i++;
+    }
+    va_end(ap);
+
+    return (result);
+}
+
+int main()
+{
+    int num1, num2;
+    int num3, num4, num5;
+
+    scanf("%d %d", &num1, &num2);
+    scanf("%d %d %d", &num3, &num4, &num5);
+
+    printf("%d\n", sum(2, num1, num2));
+    printf("%d\n", sum(3, num3, num4, num5));
+
+    return (0);
+}
